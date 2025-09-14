665f519848a02244fc30411c608710e00774ba8e
jose781 <146405410+jfernando234@users.noreply.github.com>
2025-09-14 12:29:14 -0500

    limpiar dependencias

---
+++ .angular/cache/18.2.21/SIGEWEBFE/vite/deps/@angular_material_button.js
@@ -0,0 +1,36 @@
+import {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+} from "./chunk-ZAKNDU2S.js";
+import "./chunk-52W6QTHH.js";
+import "./chunk-YHRYCBEL.js";
+import "./chunk-C432SA74.js";
+import "./chunk-4QS3KQKV.js";
+import "./chunk-L2LIFVFR.js";
+import "./chunk-GPIAFODZ.js";
+import "./chunk-EIB7IA3J.js";
+export {
+  MAT_BUTTON_CONFIG,
+  MAT_FAB_DEFAULT_OPTIONS,
+  MAT_FAB_DEFAULT_OPTIONS_FACTORY,
+  MatAnchor,
+  MatButton,
+  MatButtonModule,
+  MatFabAnchor,
+  MatFabButton,
+  MatIconAnchor,
+  MatIconButton,
+  MatMiniFabAnchor,
+  MatMiniFabButton
+};
+//# sourceMappingURL=@angular_material_button.js.map
