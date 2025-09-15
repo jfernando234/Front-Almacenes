2a075203ce99448845aa8ae60b73cd22cb5ffcfa
jose781 <146405410+jfernando234@users.noreply.github.com>
2025-09-15 01:20:12 -0500

    no se que sera

---
+++ .angular/cache/18.2.20/SIGEWEBFE/vite/deps/@angular_cdk_layout.js
@@ -0,0 +1,17 @@
+import {
+  BreakpointObserver,
+  Breakpoints,
+  LayoutModule,
+  MediaMatcher
+} from "./chunk-4YRADW36.js";
+import "./chunk-FOG3M4VB.js";
+import "./chunk-EGGEWHZM.js";
+import "./chunk-GPIAFODZ.js";
+import "./chunk-EIB7IA3J.js";
+export {
+  BreakpointObserver,
+  Breakpoints,
+  LayoutModule,
+  MediaMatcher
+};
+//# sourceMappingURL=@angular_cdk_layout.js.map
